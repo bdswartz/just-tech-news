60518200d6ebecc119d1aaf5a0f4cff4bd502612
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-03-21 10:54:11 -0400

    includes comments

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./comments');
 
 // create associations
 User.hasMany(Post, {
@@ -39,4 +40,21 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+// Comments belong to users and posts and a user and a post can have many comments
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Vote, Comment };
