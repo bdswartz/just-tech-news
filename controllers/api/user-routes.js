ba004203a488be783ebea7b62f22b4babc4a86f9
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-03-29 11:47:20 -0400

    completed MVP

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const withAuth = require('../../utils/auth');
 const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
@@ -56,7 +57,7 @@ router.get('/:id', (req, res) => {
   });
 
 // POST /api/users
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create(req.body)
     .then(dbUserData => {
@@ -115,7 +116,7 @@ router.post('/logout', (req, res) => {
 });
 
 // PUT /api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
@@ -139,7 +140,7 @@ router.put('/:id', (req, res) => {
   });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
       where: {
         id: req.params.id
